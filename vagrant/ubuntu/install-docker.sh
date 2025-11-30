7039de75450d9687c26c4bf8d0696465b9fd6e03
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-11-30 07:47:05 -0700

    update docker install script

--- vagrant/ubuntu/install-docker.sh
+++ vagrant/ubuntu/install-docker.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-apt-get update \
+apt update \
     && apt-get install -y \
         apt-transport-https \
         ca-certificates \
         curl \
         software-properties-common \
-    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - \
-    && add-apt-repository \
-        "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-        $(lsb_release -cs) \
-        stable" \
-    && apt-get update \
-    && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
+    && install -m 0755 -d /etc/apt/keyrings \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc \
+    && chmod a+r /etc/apt/keyrings/docker.asc \
+    && tee /etc/apt/sources.list.d/docker.sources <<EOF && \
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+apt update \
+    && apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
