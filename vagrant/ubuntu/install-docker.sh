c1e039c51533c33af1e5cdd9413793c33c4329b0
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-11-30 07:55:25 -0700

    update install heredoc

--- vagrant/ubuntu/install-docker.sh
+++ vagrant/ubuntu/install-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update \
+apt-get update \
     && apt-get install -y \
         apt-transport-https \
         ca-certificates \
@@ -8,12 +8,13 @@ apt update \
     && install -m 0755 -d /etc/apt/keyrings \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc \
     && chmod a+r /etc/apt/keyrings/docker.asc \
-    && tee /etc/apt/sources.list.d/docker.sources <<EOF && \
+    && (tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
-apt update \
-    && apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    ) \
+    && apt-get update \
+    && apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
